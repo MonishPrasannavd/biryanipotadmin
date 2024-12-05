6ba1576cde5718d8fd853a063844d2666abb68d1
MonishPrasannaVD <vd.monish@gmail.com>
2024-12-05 14:58:41 +0530

    add properties to application file

---
+++ src/main/java/com/example/biryanipot/BiryanipotApplication.java
@@ -0,0 +1,13 @@
+package com.example.biryanipot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BiryanipotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BiryanipotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/biryanipot/BiryanipotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.biryanipot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BiryanipotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
